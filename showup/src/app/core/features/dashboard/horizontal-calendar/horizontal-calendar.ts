b9750ec820036dff8b16d10581531419db1abf12
Adit <aditnavle@gmail.com>
2026-01-15 13:26:49 +0530

    feat: Horizontal calendar UI completed

--- showup/src/app/core/features/dashboard/horizontal-calendar/horizontal-calendar.ts
+++ showup/src/app/core/features/dashboard/horizontal-calendar/horizontal-calendar.ts
@@ -1,4 +1,14 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  AfterViewInit,
+  ElementRef,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -13,16 +23,13 @@ export interface CalendarDay {
 
 @Component({
   selector: 'app-horizontal-calendar',
-  imports: [
-    CommonModule,
-    MatButtonModule,
-    MatIconModule,
-    MatCardModule
-],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatCardModule],
   templateUrl: './horizontal-calendar.html',
   styleUrl: './horizontal-calendar.css',
 })
-export class HorizontalCalendar implements OnInit {
+export class HorizontalCalendar implements OnInit, AfterViewInit {
+  @ViewChildren('dayEl', { read: ElementRef })
+  dayElements!: QueryList<ElementRef<HTMLButtonElement>>;
 
   @Input() completedDates: string[] = [];
   @Output() dateSelected = new EventEmitter<Date>();
@@ -30,11 +37,17 @@ export class HorizontalCalendar implements OnInit {
   days: CalendarDay[] = [];
   selectedDate!: Date;
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.scrollToToday();
+    });
+  }
+
   ngOnInit(): void {
     this.buildMonth();
   }
 
-  private buildMonth(){
+  private buildMonth() {
     this.days = [];
 
     const today = new Date();
@@ -42,7 +55,7 @@ export class HorizontalCalendar implements OnInit {
     const month = today.getMonth();
     const totalDays = new Date(year, month + 1, 0).getDate();
 
-    for(let day = 1; day <= totalDays; day++){
+    for (let day = 1; day <= totalDays; day++) {
       const date = new Date(year, month, day);
       const iso = date.toISOString().split('T')[0];
 
@@ -50,10 +63,10 @@ export class HorizontalCalendar implements OnInit {
         date,
         day,
         isToday: this.sameDate(date, today),
-        isCompleted: this.completedDates.includes(iso)
+        isCompleted: this.completedDates.includes(iso),
       });
 
-      if(this.sameDate(date, today)){
+      if (this.sameDate(date, today)) {
         this.selectedDate = date;
       }
     }
@@ -62,12 +75,28 @@ export class HorizontalCalendar implements OnInit {
   }
 
   select(day: CalendarDay) {
-    console.log(day.date)
+    // console.log(day.date);
     this.selectedDate = day.date;
     this.dateSelected.emit(day.date);
   }
 
-  private sameDate(a: Date, b: Date): boolean{
+  private sameDate(a: Date, b: Date): boolean {
     return a.toDateString() === b.toDateString();
+  }
+
+  private scrollToToday() {
+    const todayIndex = this.days.findIndex((d) => d.isToday);
+    // console.log('today:', todayIndex);
+    if (todayIndex === -1) return;
+
+    const el = this.dayElements.get(todayIndex)?.nativeElement;
+    // console.log('el:', el);
+    if (!el) return;
+
+    el.scrollIntoView({
+      behavior: 'smooth',
+      inline: 'center',
+      block: 'nearest',
+    });
   }
 }
